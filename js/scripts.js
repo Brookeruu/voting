41475aef2dee577dca85dea17417c7d55f15b2b4
Brooke Perry and Victoria Martinez <victoria@>
2018-10-18 11:14:04 -0700

    write entire js;(should have done more commits)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
-$(document).ready(funtion() {
-  $().prompt(parseInt())
 
+var voteAge = function(age) {
+    if(age >= 18) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+
+
+
+$(document).ready(function() {
+  $("form#age").submit(function(event) {
+    var inputOver18 = parseInt($("input#inputAge").val());
+    var result = voteAge(inputOver18);
+
+    if(result === true) {
+      $(".overAge").show();
+      $(".underAge").hide();
+    } else {
+      $(".underAge").show();
+        $(".overAge").hide();
+    }
+
+    event.preventDefault();
+
+  });
 });
